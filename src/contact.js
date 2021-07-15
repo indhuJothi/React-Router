966e237a4f477d0814c56d29ae70850a27a25c1d
indhuJothi <indhu.palanivel@aspiresys.com>
2021-07-15 13:44:56 +0530

    First-commit

---
+++ src/contact.js
@@ -0,0 +1,11 @@
+import React from React
+
+class Contact extends React.Component{
+    render(){
+        return(
+            <h1>/Contact</h1>
+        )
+    }
+}
+
+export default Contact
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import About from './about';
+import Contact from './contact';
+import reportWebVitals from './reportWebVitals';
+import { Route, Router } from 'react-router-dom';
+
+const routing=(
+  <Router>
+    <div>
+      
+        <h1>React Router Example</h1>
+        <Route path='/' component={App}/>
+        <Route path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/>
+     
+    </div>
+  </Router>
+)
+ReactDOM.render(
+  routing,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
